a887f3fe23dc1b2d71a24b749f3a7e5f04215a11
peaceapple <astonreba@gmail.com>
2023-09-25 13:22:08 +0300

    Add link routes

--- app/home/page.js
+++ app/home/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link"
+
 export default function Page() {
     return (
       <>
         <p>Welcome to Home!</p>
+        <h2><Link href='/blog'>Blog</Link></h2>
+        <h2><Link href='/settings'>Setting</Link></h2>
+        <h2><Link href='/conference'>Conference</Link></h2>
       </>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link"
+
 export default function Page() {
   return (
     <>
       <p>Welcome to Globext!</p>
+      <Link href='/home'>Home</Link>
     </>
   )
 }
