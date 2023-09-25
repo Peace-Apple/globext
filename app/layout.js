498a54687bcddbbdb2c554729c40286bc8374460
peaceapple <astonreba@gmail.com>
2023-09-25 14:14:12 +0300

    Add header to root layout page

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google'
+import Link from 'next/link';
+import styles from './rootStyle.module.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +12,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <header>
+          <h1>
+            <Link className={styles.homeLink} href={"/home"}>
+              GLOBOMANTICS
+            </Link>
+          </h1>
+          <h3>
+            <Link className={styles.menuBarLinks} href="/blog">
+              Blog
+            </Link>
+          </h3>
+          <h3>
+            <Link className={styles.menuBarLinks} href="/settings">
+              Settings
+            </Link>
+          </h3>
+          <h3>
+            <Link className={styles.menuBarLinks} href="/conference">
+              Conference
+            </Link>
+          </h3>
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
