2cd27c5a2c5387a2438027d845b62ab267f6a413
peaceapple <astonreba@gmail.com>
2023-09-25 13:33:58 +0300

    Add link back to conference

--- app/conference/sessions/page.js
+++ app/conference/sessions/page.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Page() {
     return (
       <>
-        <p>Welcome to Speakers!</p>
+        <p>Welcome to Session!</p>
         <h2><Link href='/conference'>Back to Conference</Link></h2>
       </>
     )
